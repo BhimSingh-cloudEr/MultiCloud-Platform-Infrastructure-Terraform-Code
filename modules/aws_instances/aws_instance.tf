7308863aa2a81ce54bf2b07f45247bd2328b8127
Bhim Singh <160112851+BhimSingh-cloudEr@users.noreply.github.com>
2025-04-03 16:04:05 +0530

    Update aws_instance.tf

--- modules/aws_instances/aws_instance.tf
+++ modules/aws_instances/aws_instance.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_version = ">= 1.0.0"
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.25"
-#     }
-#   }
-# }
+ terraform {
+   required_version = ">= 1.0.0"
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 4.25"
+     }
+   }
+ }
 
 resource "aws_instance" "infra_developer_instance"{
   ami               = var.ami
