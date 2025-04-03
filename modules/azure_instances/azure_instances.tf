e5f6bdab5cd224de6ca00457ba177e91787bd968
Bhim Singh <160112851+BhimSingh-cloudEr@users.noreply.github.com>
2025-04-03 16:05:22 +0530

    Update azure_instances.tf

--- modules/azure_instances/azure_instances.tf
+++ modules/azure_instances/azure_instances.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_version = ">= 1.0.0"
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~> 3.17"
-#     }
-#   }
-# }
+ terraform {
+   required_version = ">= 1.0.0"
+   required_providers {
+      azurerm = {
+       source  = "hashicorp/azurerm"
+       version = "~> 3.17"
+     }
+   }
+ }
 
 # Removed deprecated argument "skip_provider_registration"
 
